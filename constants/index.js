c63966ea6153bb3a82d662e3e82452feca683b2a
SHAADAAB <99734040+dolbach@users.noreply.github.com>
2023-07-31 00:57:34 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xed2fa9560a3ae08457daf882dd082dba6fa8e291";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
